020ea2db3e4a2fbd32705f2897a40504e2347485
nachodev <159072179+nachodev30@users.noreply.github.com>
2024-10-27 13:24:26 +0000

    Generador de Excusas

--- src/app.js
+++ src/app.js
@@ -6,6 +6,31 @@ import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
 window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
+  let who = ["The dog", "My grandma", "The mailman", "My bird"];
+  let action = ["ate", "peed", "crushed", "broke"];
+  let what = ["my homework", "my phone", "the car"];
+  let when = [
+    "before the class",
+    "when I was sleeping",
+    "while I was exercising",
+    "during my lunch",
+    "while I was praying"
+  ];
+
+  const getRandomElement = ListElements => {
+    let element = "";
+    element = ListElements[Math.floor(Math.random() * ListElements.length)];
+    return element;
+  };
+  getRandomElement(action);
+
+  let excuseGen = document.querySelector("#excuse");
+  console.log(excuseGen);
+  excuseGen.innerHTML =
+    getRandomElement(who) +
+    " " +
+    getRandomElement(action) +
+    " " +
+    getRandomElement(what);
+  +" " + getRandomElement(when);
 };
